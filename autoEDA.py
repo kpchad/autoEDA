3395dcc26442dfa81f8fcb0d73aae69b6f8fbcb8
kpchad <kylepchad@gmail.com>
2016-11-26 12:16:10 -0800

    describe features found

--- autoEDA.py
+++ autoEDA.py
@@ -6,3 +6,17 @@ Created on Sat Nov 26 11:05:30 2016
 @author: kylechadwick
 """
 
+import pandas as pd
+
+#load dataset
+df = pd.read_csv('direct_marketing.csv')
+
+#print feature names and describe each series
+print(df.columns)
+print(df.describe())
+
+#print each features datatype
+for i in df.columns:
+    print('column ' + i + ' is of type:')
+    print(df.ix[:,i].dtype)
+
