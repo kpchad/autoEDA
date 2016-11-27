7d0186b9058805a598de0b45f9ff2ed420c17db2
kpchad <kylepchad@gmail.com>
2016-11-27 13:54:19 -0800

    deal with nans

--- autoEDA.py
+++ autoEDA.py
@@ -9,14 +9,21 @@ Created on Sat Nov 26 11:05:30 2016
 import pandas as pd
 
 #load dataset
-df = pd.read_csv('direct_marketing.csv')
+df = pd.read_csv('Meteorite_Landings.csv')
 
-#print feature names and describe each series
+#print feature names and describe each
 print(df.columns)
 print(df.describe())
 
-#print each features datatype
+#print details of each feature
 for i in df.columns:
-    print('column ' + i + ' is of type:')
-    print(df.ix[:,i].dtype)
+    print('column ' + i + ' is of type:', df.ix[:,i].dtype)
+    print('number of nans in series:', sum(df.ix[:,i].isnull()))
+    
+#drop all rows with nans
+df = df.dropna(axis=0).reset_index()
+
+#replace all nans with zero
+#df = df.fillna(0)
+
 
